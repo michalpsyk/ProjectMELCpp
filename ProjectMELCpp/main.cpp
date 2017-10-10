2730491a4d6d82927cc5749e4150ba67da0d6a84
Michał Psyk <michalpsyk@gmail.com>
2017-10-10 19:21:01 +0200

    Changed "." to "!"

--- ProjectMELCpp/main.cpp
+++ ProjectMELCpp/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	cout << "Witaj w bazie danych biura matrymonialnego Venus.\n" << endl;
+	cout << "Witaj w bazie danych biura matrymonialnego Venus!\n" << endl;
 	Database* bazaDanych = new Database;
 	bazaDanych->menu();
 	delete bazaDanych;
